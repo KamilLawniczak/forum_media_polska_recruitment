3868be7cdeb6cb50a2dd057760429aaa0a782db4
Kamil Lawniczak <klawniczak2018@outlook.com>
2020-03-31 08:59:53 +0200

    Add-Migration Initial

---
+++ src/chat_app.domain/Migrations/20200331062644_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace chat_app.domain.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: true),
+                    PasswordHash = table.Column<byte[]>(maxLength: 64, nullable: true),
+                    PasswordSalt = table.Column<byte[]>(maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatUsers_Name",
+                table: "ChatUsers",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatUsers");
+        }
+    }
+}
