fc0dc29575cfd230e484202bb62a711d546855e0
Kamil Lawniczak <klawniczak2018@outlook.com>
2020-03-31 03:46:23 +0200

    Create SecurePasswordService
    Service which will generate password hashes for new users, and challenge passwords for existing users.

---
+++ src/chat_app.domain/User/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chat_app.domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
