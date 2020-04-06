8900ab4c0e7ee0dfa0401fb0d51997b4a464589a
Kamil Lawniczak <klawniczak2018@outlook.com>
2020-04-06 08:01:46 +0200

    Add-Migration Public Message Entity

---
+++ src/chat_app.domain/Migrations/20200406053532_PublicMessagesEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace chat_app.domain.Migrations
+{
+    public partial class PublicMessagesEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PublicMessages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    SenderId = table.Column<Guid>(nullable: false),
+                    When = table.Column<DateTimeOffset>(nullable: false),
+                    Text = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PublicMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PublicMessages_When",
+                table: "PublicMessages",
+                column: "When");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PublicMessages");
+        }
+    }
+}
