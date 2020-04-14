ac0524a0d0d2a9d6203c862bd2bc5980f9e001eb
Kamil Lawniczak <klawniczak2018@outlook.com>
2020-04-14 10:37:25 +0200

    Enable user to user message sending

--- src/chat_app.web/Controllers/ChatController.cs
+++ src/chat_app.web/Controllers/ChatController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using chat_app.domain;
 using chat_app.web.Hubs;
@@ -15,6 +16,12 @@ namespace chat_app.web.Controllers
     [Authorize]
     public class ChatController : Controller
     {
+        private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+
         private readonly MessageService _messageService;
         private readonly OnlineUsers _onlineUsers;
         private readonly IHubContext<ChatHub> _hubContext;
@@ -26,9 +33,10 @@ namespace chat_app.web.Controllers
             _hubContext = hubContext;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var id = Guid.Parse(HttpContext.User.Claims.Single (x => x.Type == "UserId").Value);
+            var id = Guid.Parse (HttpContext.User.Claims.Single (x => x.Type == "UserId").Value);
             var name = HttpContext.User.Claims.Single (x => x.Type == "UserName").Value;
 
             if (!_onlineUsers.IsOnline (id))
@@ -37,7 +45,7 @@ namespace chat_app.web.Controllers
             }
 
             var users = _onlineUsers.List.ToList ();
-            var messages = await _messageService.GetTopPublicMessages ();
+            var messages = await _messageService.GetPublicMessages ();
 
             var conversations = new List<Conversation>
             {
@@ -53,49 +61,95 @@ namespace chat_app.web.Controllers
                         Content = x.Text
                     }).ToList()
                 },
-
-                new Conversation
-                {
-                    Id = "Test",
-                    Messages = messages.Select(x => new ChatMessageViewModel
-                    {
-                        ConversationId = "Public",
-                        SenderId = x.SenderId,
-                        Sended = x.When,
-                        Received = x.When,
-                        Content = x.Text
-                    }).ToList()
-                },
             };
 
-            conversations[0].Messages.Add (new ChatMessageViewModel { SenderId = Guid.NewGuid(), Content = "hahaha"});
-            conversations[1].Messages.Add (new ChatMessageViewModel { SenderId = Guid.NewGuid(), Content = "dsfdfs"});
             var vm = new ChatViewModel
             {
                 OnlineUsers = users,
-                Conversations =  conversations
+                Conversations = conversations
             };
 
-            return View(vm);
+            return View (vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPrivateMessagesPage([FromQuery]PrivateMessagesPageQueryModel privateMessagesPageQueryModel)
+        {
+            var currentUserId = GetUserId ();
+            var messages = await _messageService.GetPrivateMessages (
+                currentUserId,
+                privateMessagesPageQueryModel.InterlocutorId,
+                privateMessagesPageQueryModel.Count,
+                privateMessagesPageQueryModel.Skip);
+
+            var result = messages.Select (m => new ChatMessageViewModel
+            {
+                ConversationId = privateMessagesPageQueryModel.InterlocutorId.ToString (),
+                SenderId = m.SenderId,
+                Sended = m.Sended,
+                Received = m.Received,
+                Content = m.Text
+            });
+
+            return Json (result, _jsonSerializerOptions);
         }
 
         [HttpPost]
         public async Task<IActionResult> SendPublicMessage([FromBody] SendPublicMessageModel sendPublicMessageModel)
         {
-            var msg = await _messageService.AddPublicMessage (GetUserId(), sendPublicMessageModel.Content);
+            var msg = await _messageService.AddPublicMessage (GetUserId (), sendPublicMessageModel.Content);
 
-            await _hubContext.Clients.All.SendAsync ("ReceivePublicMessage", new ChatMessageViewModel
-            {
-                ConversationId = "Public",
-                Content = msg.Text,
-                SenderId = msg.SenderId,
-                Received = msg.When,
-                Sended = msg.When
-            });
+            await _hubContext.Clients.All.SendAsync ("ReceivePublicMessage", ChatMessageViewModel.ForPublic(msg));
+
+            return Ok ();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendPrivateMessage([FromBody] SendPrivateMessageModel sendPrivateMessageModel)
+        {
+            var currentUserId = GetUserId ();
+
+            var privateMessage = await _messageService.AddPrivateMessage (
+                                        currentUserId,
+                                        sendPrivateMessageModel.ReceiverId,
+                                        sendPrivateMessageModel.Sended,
+                                        sendPrivateMessageModel.Content);
+
+            var todoSender = CreateMessageSendingTasks (
+                _onlineUsers.GetChatHubConnections (currentUserId),
+                ChatMessageViewModel.ForMessageSender (privateMessage),
+                _hubContext);
+
+            var todoReceiver = CreateMessageSendingTasks (
+                _onlineUsers.GetChatHubConnections (privateMessage.ReceiverId),
+                ChatMessageViewModel.ForMessageReceiver (privateMessage),
+                _hubContext);
+
+            await Task.WhenAll (todoSender.Concat (todoReceiver));
+
+            return Ok ();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PrivateMessageReceived([FromBody] PrivateMessageReceivedModel privateMessageReceivedModel)
+        {
+            await _messageService.UpdateReceivedTime (privateMessageReceivedModel.MessageId, privateMessageReceivedModel.Received);
 
             return Ok ();
         }
 
         private Guid GetUserId() => Guid.Parse (HttpContext.User.Claims.Single (x => x.Type == "UserId").Value);
+
+        private static IEnumerable<Task> CreateMessageSendingTasks(
+            IEnumerable<string> hubConnections,
+            ChatMessageViewModel messageViewModel,
+            IHubContext<ChatHub> hubContext)
+        {
+            return hubConnections.Select (
+                x => Task.Run (async () => {
+                    await hubContext.Clients.Client (x).SendAsync ("ReceivePrivateMessage", messageViewModel);
+                    return true;
+                }));
+        }
     }
 }
--- src/chat_app.web/wwwroot/js/chatViewModel.js
+++ src/chat_app.web/wwwroot/js/chatViewModel.js
@@ -1,7 +1,27 @@
 ﻿class conversation {
-    constructor(id, messages) {
+    constructor(id, name, currentUserId, messages) {
         this.id = ko.observable(id);
+        this.currentUserId = ko.observable(currentUserId);
+        this.name = ko.observable(name);
         this.messages = ko.observableArray(messages);
+    }
+
+    getMessagesPage = (onlineUsers) => {
+        const count = 20;
+        const skip = this.messages().length;
+
+        let url = new URL("/Chat/GetPrivateMessagesPage", window.location.origin);
+        url.searchParams.append("interlocutorId", this.id());
+        url.searchParams.append("skip", skip.toString());
+        url.searchParams.append("count", count.toString());
+
+        fetch(url).then(response => response.json().then(data => {
+            const mesages = data;
+            mesages.forEach(m => {
+                matchUserName(m, onlineUsers)
+                this.messages.push(m);
+            });
+        })).catch(err => console.log(err));
     }
 }
 
@@ -13,7 +33,7 @@ class onlineUser {
 }
 
 class chatViewModel {
-    constructor(onlineUsers, conversations) {
+    constructor(onlineUsers, conversations, currentUserId) {
         this.onlineUsers = ko.observableArray(onlineUsers);
         this.conversations = ko.observableArray(conversations);
         this.typedMessage = ko.observable("");
@@ -23,34 +43,68 @@ class chatViewModel {
             x.selectedConversation = this.selectedConversation
             x.isSelected = ko.computed(() => x.id() === this.selectedConversation().id(), x);
         });
+
+        this.currentUserId = ko.observable(currentUserId);
         this.canSendMessage = true;//ko.computed(() => !this.typedMessage() || this.typedMessage().length === 0);
     }
 
-    sendMessage() {
+    sendMessage = () => {
         if (this.selectedConversation().id() === "Public") {
             this.sendPublicMessage();
+        } else {
+            this.sendPrivateMessage();
         }
     }
 
-    sendPublicMessage() {
+    sendPublicMessage = () => {
         const data = { Content: this.typedMessage() }
-        const response = fetch("/Chat/SendPublicMessage", {
+        fetch("/Chat/SendPublicMessage", {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         }).then(data => console.log("sended public message"))
-          .catch(err => console.log(err));
+            .catch(err => console.log(err));
     }
 
-    addMessage(message) {
+    sendPrivateMessage = () => {
+        const data = {
+            ReceiverId: this.selectedConversation().id(),
+            Sended: new Date().toISOString(),
+            Content: this.typedMessage()
+        };
+
+        fetch("/Chat/SendPrivateMessage", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(data => console.log("sended private message"))
+            .catch(err => console.log(err));
+    }
+
+    addPublicMessage = (message) => {
         const conversation = this.conversations().find(x => x.id() === "Public");
+        this.selectedConversation = conversation;
         conversation.messages.push(message);
         $("#messages").scroll();
     }
 
-    addUser(usr) {
+    addPrivateMessage = (message) => {
+        let conversation = this.conversations().find(x => x.id() === message.conversationId);
+
+        if (conversation === undefined) {
+            const name = this.onlineUsers.find(x => x.id() === message.conversationId);
+            conversation = new conversation(message.conversationId, name, this.currentUserId(), []);
+            conversation.getMessagesPage();
+        }
+        conversation.messages.push(message);
+        this.selectedConversation = conversation;
+    }
+
+    addUser = (usr) => {
         let id = usr.id;
         let name = usr.name;
 
@@ -60,12 +114,19 @@ class chatViewModel {
         this.onlineUsers.push(user);
     }
 
-    removeUser(id) {
+    removeUser= (id) => {
         const toRemove = this.onlineUsers().find(x => x.userId() === id);
 
         if (toRemove === undefined) return;
 
         this.onlineUsers.remove(toRemove);
+    }
+
+    openConversation = (interLocutor) => {
+        const conv = new conversation(interLocutor.userId(), interLocutor.name(), this.currentUserId(), []);
+        conv.getMessagesPage(this.onlineUsers());
+        conv.isSelected = ko.computed(() => conv.id() === this.selectedConversation().id(), conv)
+        this.conversations.push(conv);
     }
 }
 
@@ -77,5 +138,5 @@ function matchUserName(message, onlineUsers) {
     } else {
         message.userName = user.name;
     }
-    
+
 }
